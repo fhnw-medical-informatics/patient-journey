5f3332b4c69c4912d20b43cd51b33bb87fd0a9f7
Rahel Lüthy <rahel.luethy@fhnw.ch>
2022-02-11 11:39:57 +0100

    #5: Handle empty data loading

--- patient-journey-client/src/data/dataSlice.test.ts
+++ patient-journey-client/src/data/dataSlice.test.ts
@@ -5,6 +5,7 @@ const globalAny = global as any
 
 describe('dataSlice', () => {
   const successUrl = 'success-url'
+  const successUrlEmpty = 'success-url-empty'
   const errorUrl = 'error-url'
 
   beforeEach(() => {
@@ -13,6 +14,10 @@ describe('dataSlice', () => {
         case `${successUrl}`:
           return Promise.resolve({
             text: () => Promise.resolve('Id,Col_1,Col_2\nId1,Cell_11,Cell_12\n\nId2,Cell_21,Cell_22'),
+          })
+        case `${successUrlEmpty}`:
+          return Promise.resolve({
+            text: () => Promise.resolve(''),
           })
         case `${errorUrl}`:
           throw new Error('Boom!')
@@ -34,6 +39,17 @@ describe('dataSlice', () => {
     expect(patients.fields).toEqual(['Id', 'Col_1', 'Col_2'])
     expect(patients.rows[0].id).toEqual('Id1')
     expect(patients.rows[1].id).toEqual('Id2')
+  })
+
+  it('loadData loading-complete empty data', async () => {
+    const store = createStore()
+    await loadData(successUrlEmpty)(store.dispatch)
+
+    const data = store.getState().data
+    expect(data.type).toEqual('loading-complete')
+    const patients = (data as DataStateLoadingComplete).patientData
+    expect(patients.rows).toEqual([])
+    expect(patients.fields).toEqual([])
   })
 
   it('loadData loading-failed', async () => {
--- patient-journey-client/src/data/dataSlice.ts
+++ patient-journey-client/src/data/dataSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import { AppDispatch } from '../store'
 import * as csvParser from 'papaparse'
+import { ParseResult } from 'papaparse'
 
 type DataStateLoadingPending = Readonly<{
   type: 'loading-pending'
@@ -73,18 +74,29 @@ export const loadData =
       const csv = await response.text()
       // use header = false to get string[][] rather than JSON -> extracting header fields ourselves
       const result = csvParser.parse<string[]>(csv, { header: false, skipEmptyLines: true })
-      const data: PatientData = {
-        fields: result.data[0],
-        rows: result.data.slice(1).map((row: string[]) => {
-          return {
-            id: row[0] as PatientId,
-            values: row,
-          }
-        }),
-      }
+      const data = createData(result)
       dispatch(loadingDataComplete(data))
     } catch (e) {
       console.error(e)
       dispatch(loadingDataFailed('Error fetching data'))
     }
   }
+
+const createData = (result: ParseResult<string[]>): PatientData => {
+  if (result.data.length === 0) {
+    return {
+      fields: [],
+      rows: [],
+    }
+  } else {
+    return {
+      fields: result.data[0],
+      rows: result.data.slice(1).map((row: string[]) => {
+        return {
+          id: row[0] as PatientId,
+          values: row,
+        }
+      }),
+    }
+  }
+}
