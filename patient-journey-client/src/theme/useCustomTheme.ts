5d21a79a881cd9d670e3458ec55d0a640ca82d0f
Rahel Lüthy <rahel.luethy@fhnw.ch>
2022-02-14 12:06:07 +0100

    #9: Use same color for hovering & selection

--- patient-journey-client/src/theme/useCustomTheme.ts
+++ patient-journey-client/src/theme/useCustomTheme.ts
@@ -1,80 +1,55 @@
-import { alpha, createTheme, Theme } from '@mui/material'
+import { createTheme, Theme } from '@mui/material'
 import { useAppSelector } from '../store'
 import { deepPurple, yellow } from '@mui/material/colors'
+import { AppTheme } from './themeSlice'
 
 declare module '@mui/material/styles' {
   interface Theme {
     entityColors: {
       selected: string
-      hovered: string
     }
   }
+
   // allow configuration using `createTheme`
+  // noinspection JSUnusedGlobalSymbols
   interface ThemeOptions {
     entityColors?: {
       selected?: string
-      hovered?: string
     }
   }
 }
 
-const LIGHT_SELECTED_COLOR = yellow[200]
-const LIGHT_HOVERED_COLOR = alpha(LIGHT_SELECTED_COLOR, 0.5)
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-  entityColors: {
-    selected: LIGHT_SELECTED_COLOR,
-    hovered: LIGHT_HOVERED_COLOR,
-  },
-  components: {
-    MuiTableRow: {
-      styleOverrides: {
-        root: {
-          '&.MuiTableRow-hover': {
-            '&:hover': {
-              backgroundColor: LIGHT_HOVERED_COLOR,
+const createCustomTheme = (mode: AppTheme, selectionColor: string) =>
+  createTheme({
+    palette: {
+      mode,
+    },
+    entityColors: {
+      selected: selectionColor,
+    },
+    components: {
+      MuiTableRow: {
+        styleOverrides: {
+          root: {
+            '&.MuiTableRow-hover': {
+              '&:hover': {
+                backgroundColor: selectionColor,
+              },
             },
-          },
-          '&.Mui-selected': {
-            backgroundColor: LIGHT_SELECTED_COLOR,
+            '&.Mui-selected': {
+              backgroundColor: selectionColor,
+            },
           },
         },
       },
     },
-  },
-})
+  })
 
-const DARK_SELECTED_COLOR = deepPurple[400]
-const DARK_HOVERED_COLOR = alpha(DARK_SELECTED_COLOR, 0.5)
+const LIGHT_SELECTION_COLOR = yellow[200]
+const DARK_SELECTION_COLOR = deepPurple[400]
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-  entityColors: {
-    selected: DARK_SELECTED_COLOR,
-    hovered: DARK_HOVERED_COLOR,
-  },
-  components: {
-    MuiTableRow: {
-      styleOverrides: {
-        root: {
-          '&.MuiTableRow-hover': {
-            '&:hover': {
-              backgroundColor: DARK_HOVERED_COLOR,
-            },
-          },
-          '&.Mui-selected': {
-            backgroundColor: DARK_SELECTED_COLOR,
-          },
-        },
-      },
-    },
-  },
-})
+const lightTheme = createCustomTheme('light', LIGHT_SELECTION_COLOR)
+const darkTheme = createCustomTheme('dark', DARK_SELECTION_COLOR)
 
 export const useCustomTheme = () =>
   useAppSelector<Theme>((s) => {
