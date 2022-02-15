c6d1cf80dfe9d4c74b7d3fa97a39b05304d25679
Rahel Lüthy <rahel.luethy@fhnw.ch>
2022-02-15 11:18:37 +0100

    #9: Extract re-usable patient interaction hook

--- patient-journey-client/src/data/containers/table/PatientDataTable.tsx
+++ patient-journey-client/src/data/containers/table/PatientDataTable.tsx
@@ -1,34 +1,19 @@
 import { PatientDataTable as PatientDataTableComponent } from '../../components/table/PatientDataTable'
-import { PatientData, PatientId, PatientIdNone, setHoveredPatient, setSelectedPatient } from '../../dataSlice'
-import React, { useCallback } from 'react'
-import { useAppDispatch } from '../../../store'
+import { PatientData } from '../../dataSlice'
+import React from 'react'
+import { usePatientInteraction } from '../../hooks'
 
 interface Props {
   readonly data: PatientData
 }
 
 export const PatientDataTable = ({ data }: Props) => {
-  const dispatch = useAppDispatch()
-
-  const onPatientClick = useCallback(
-    (id: PatientId) => {
-      dispatch(setSelectedPatient(data.selectedPatient === id ? PatientIdNone : id))
-    },
-    [data.selectedPatient, dispatch]
-  )
-
-  const onPatientHover = useCallback(
-    (id: PatientId) => {
-      dispatch(setHoveredPatient(id))
-    },
-    [dispatch]
-  )
-
+  const { selectedPatient, onPatientClick, onPatientHover } = usePatientInteraction()
   return (
     <PatientDataTableComponent
       columns={data.fields}
       patients={data.allPatients}
-      selectedPatient={data.selectedPatient}
+      selectedPatient={selectedPatient}
       onPatientClick={onPatientClick}
       onPatientHover={onPatientHover}
     />
--- patient-journey-client/src/data/hooks.ts
+++ patient-journey-client/src/data/hooks.ts
@@ -1,7 +1,43 @@
-import { useAppSelector } from '../store'
+import { useAppDispatch, useAppSelector } from '../store'
 import { selectHoveredPatient, selectPatientData, selectSelectedPatient } from './selectors'
-import { PatientId } from './dataSlice'
+import { PatientId, PatientIdNone, setHoveredPatient, setSelectedPatient } from './dataSlice'
+import { useCallback } from 'react'
 
 export const usePatientData = () => useAppSelector(selectPatientData)
 export const useSelectedPatient = (): PatientId => useAppSelector(selectSelectedPatient)
 export const useHoveredPatient = () => useAppSelector(selectHoveredPatient)
+
+export interface PatientInteraction {
+  readonly selectedPatient: PatientId
+  readonly hoveredPatient: PatientId
+  readonly onPatientClick: (id: PatientId) => void
+  readonly onPatientHover: (id: PatientId) => void
+}
+
+export const usePatientInteraction = (): PatientInteraction => {
+  const selectedPatient = useSelectedPatient()
+  const hoveredPatient = useHoveredPatient()
+
+  const dispatch = useAppDispatch()
+
+  const onPatientClick = useCallback(
+    (id: PatientId) => {
+      dispatch(setSelectedPatient(selectedPatient === id ? PatientIdNone : id))
+    },
+    [selectedPatient, dispatch]
+  )
+
+  const onPatientHover = useCallback(
+    (id: PatientId) => {
+      dispatch(setHoveredPatient(id))
+    },
+    [dispatch]
+  )
+
+  return {
+    selectedPatient,
+    hoveredPatient,
+    onPatientClick,
+    onPatientHover,
+  }
+}
