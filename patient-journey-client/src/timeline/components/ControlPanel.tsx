0468dc31e8002c8ab895c0688e1b18c06226098b
Mirco Blaser <denacem@gmail.com>
2022-03-03 16:48:53 +0100

    #10 Small fixes

--- patient-journey-client/src/timeline/components/ControlPanel.tsx
+++ patient-journey-client/src/timeline/components/ControlPanel.tsx
@@ -1,108 +1,149 @@
 import { FormControl, Typography } from '@material-ui/core'
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import { InputLabel, MenuItem, Select, SelectChangeEvent, Tooltip } from '@mui/material'
+import React, { useState } from 'react'
+import { makeStyles } from '../../utils'
+import { FormControlLabel, Grid, MenuItem, Select, SelectChangeEvent, Switch, Tooltip } from '@mui/material'
 import HelpIcon from '@mui/icons-material/Help'
 import { Box } from '@mui/system'
 import { TimelineType } from '../timelineSlice'
 import { useAppSelector } from '../../store'
 
-const useStyles = makeStyles({
-  controlPanel: {
+const useStyles = makeStyles()((theme) => ({
+  root: {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: '50% 50%',
     alignItems: 'center',
   },
+  toolbar: {
+    flex: 1,
+  },
+  timelineContainer: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(0.5),
+  },
+  timeline: {
+    display: 'grid',
+    height: '100%',
+    width: '100%',
+  },
   paper: {
     padding: 15,
     marginRight: 5,
   },
-  hci: {
-    color: '#9e9e9e',
-    lineHeight: 0.8,
-    '& td': {
-      paddingRight: 10,
-    },
-  },
-})
+}))
 
 interface ControlPanelProps {
   onSetTimelineType: (type: TimelineType) => void
 }
 
 export const ControlPanel = ({ onSetTimelineType }: ControlPanelProps) => {
-  const classes = useStyles()
+  const { classes } = useStyles()
+  const [enableClustering, setEnableClustering] = useState(true)
 
   return (
-    <div className={classes.controlPanel}>
-      <Box className={classes.paper}>
-        <ConfigOptions onSetTimelineType={onSetTimelineType} />
-      </Box>
-      <Box className={classes.paper} alignItems="right">
-        <Tooltip title={<KeyboardShortcuts />} placement="top">
-          <HelpIcon />
-        </Tooltip>
-      </Box>
+    <div className={classes.root}>
+      <Grid container alignItems="center">
+        <Grid item>
+          <Box className={classes.paper}>
+            <ConfigType onSetTimelineType={onSetTimelineType} />
+          </Box>
+        </Grid>
+        <Grid item>
+          <FormControlLabel
+            control={
+              <Switch
+                checked={enableClustering}
+                onChange={() => setEnableClustering((current) => !current)}
+                color="primary"
+              />
+            }
+            label="Cluster Tickets"
+          />
+        </Grid>
+        <Grid item>
+          <Box className={classes.paper} alignItems="right">
+            <Tooltip
+              title={
+                <div>
+                  <Typography color="inherit">Timline keyboard shortcuts</Typography>
+                  <table>
+                    <tbody>
+                      <tr>
+                        <td>Zoom In:</td>
+                        <td>Click</td>
+                      </tr>
+                      <tr>
+                        <td>Zoom Out:</td>
+                        <td>Alt + Click</td>
+                      </tr>
+                      <tr>
+                        <td>Zoom Custom:</td>
+                        <td>Shift + Click + Drag</td>
+                      </tr>
+                      <tr>
+                        <td>Pan:</td>
+                        <td>Click + Drag</td>
+                      </tr>
+                      <tr>
+                        <td>Reset:</td>
+                        <td>Esc</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              }
+              placement="top"
+              arrow
+            >
+              <HelpIcon />
+            </Tooltip>
+          </Box>
+        </Grid>
+      </Grid>
     </div>
   )
 }
 
-const KeyboardShortcuts = () => {
-  const classes = useStyles()
-  return (
-    <Typography className={classes.hci} component={'div'}>
-      <table>
-        <tbody>
-          <tr>
-            <td>Zoom In:</td>
-            <td>Click</td>
-          </tr>
-          <tr>
-            <td>Zoom Out:</td>
-            <td>Alt + Click</td>
-          </tr>
-          <tr>
-            <td>Zoom Custom:</td>
-            <td>Shift + Click + Drag</td>
-          </tr>
-          <tr>
-            <td>Pan:</td>
-            <td>Click + Drag</td>
-          </tr>
-          <tr>
-            <td>Reset:</td>
-            <td>Esc</td>
-          </tr>
-        </tbody>
-      </table>
-    </Typography>
-  )
-}
-
-const ConfigOptions = ({ onSetTimelineType }: ControlPanelProps) => {
+const ConfigType = ({ onSetTimelineType }: ControlPanelProps) => {
   const timelineState = useAppSelector((state) => state.timeline)
 
   const onChangeView = (event: SelectChangeEvent) => {
-    console.log(event)
     if (event.target.value === 'timestamp') {
       onSetTimelineType('timestamp')
     }
     if (event.target.value === 'date of birth') {
       onSetTimelineType('date of birth')
-      console.log(event.target.value)
-      console.log(timelineState)
     }
   }
 
   return (
     <FormControl>
-      <InputLabel id="demo-simple-select-label">Column</InputLabel>
+      <Select id="demo-simple-select" value={timelineState.type} label="Age" onChange={onChangeView} size="small">
+        <MenuItem value={'date of birth'}>Date Of Birth</MenuItem>
+        <MenuItem value={'timestamp'}>Timestamp</MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+/* const ConfigCluster = ({ onSetTimelineType }: ControlPanelProps) => {
+  const timelineState = useAppSelector((state) => state.timeline)
+
+  const onChangeView = (event: SelectChangeEvent) => {
+    if (event.target.value === 'timestamp') {
+      onSetTimelineType('timestamp')
+    }
+    if (event.target.value === 'date of birth') {
+      onSetTimelineType('date of birth')
+    }
+  }
+
+  return (
+    <FormControl>
       <Select
-        labelId="demo-simple-select-label"
         id="demo-simple-select"
         value={timelineState.type}
-        label="View"
+        label="Age"
         onChange={onChangeView}
         size="small"
       >
@@ -111,4 +152,4 @@ const ConfigOptions = ({ onSetTimelineType }: ControlPanelProps) => {
       </Select>
     </FormControl>
   )
-}
+} */
