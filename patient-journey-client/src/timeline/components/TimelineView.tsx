b1c353f909747e0f92da43bf0e0af849ffa690be
Mirco Blaser <denacem@gmail.com>
2022-03-03 13:48:03 +0100

    #10 Add Date Of Birth and Timestamp to Timeline

--- patient-journey-client/src/timeline/components/TimelineView.tsx
+++ patient-journey-client/src/timeline/components/TimelineView.tsx
@@ -1,52 +1,58 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { PatientData } from '../../data/patients'
 import { Timeline as SVGTimeline, LaneDisplayMode } from 'react-svg-timeline'
 import AutoSizer, { Size } from 'react-virtualized-auto-sizer'
+import { Events, Lanes, TimelineState } from '../timelineSlice'
 
 interface TimelineProps {
   readonly data: PatientData
   dateFormat: (ms: number) => string
   laneDisplayMode: LaneDisplayMode
+  timelineState: TimelineState
 }
 
-export const TimelineView = ({ data, dateFormat, laneDisplayMode }: TimelineProps) => {
+export const TimelineView = ({ data, dateFormat, laneDisplayMode, timelineState }: TimelineProps) => {
   const patients = data.allPatients
   const columns = data.columns
-  let rawEvents: (
-    | {
-        eventId: string
-        startTimeMillis: number
-        laneId: string
-        endTimeMillis?: undefined
-      }
-    | {
-        eventId: string
-        startTimeMillis: number
-        endTimeMillis: number
-        laneId: string
-      }
-  )[] = []
+  const [rawEvents, setRawEvents] = useState<Events>([])
+  const [lanes, setLanes] = useState<Lanes>([])
 
-  patients.forEach((patient) => {
-    console.log(patient)
-    columns.forEach((column) => {
-      if (column.name === 'Timestamp') {
-        const startTimeMillis = Number(patient.values[column.index])
-        rawEvents.push({ eventId: patient.pid, laneId: 'timestamp', startTimeMillis: startTimeMillis })
-      }
-    })
-  })
+  useEffect(() => {
+    let lanes: Lanes = []
+    let rawEvents: Events = []
 
-  console.log(rawEvents)
-
-  //const { lanes, events } = datasetJSON
-
-  const lanes = [
-    {
-      laneId: 'timestamp',
-      label: 'Timestamp',
-    },
-  ]
+    if (timelineState.type === 'date of birth') {
+      lanes.push({
+        laneId: 'date of birth',
+        label: 'Date Of Birth',
+      })
+      patients.forEach((patient) => {
+        columns.forEach((column) => {
+          if (column.name === 'Date Of Birth') {
+            const dateOfBirth = patient.values[column.index].split('.')
+            const date = new Date(Number(dateOfBirth[2]), Number(dateOfBirth[1]) - 1, Number(dateOfBirth[0]))
+            const startTimeMillis = date.getTime()
+            rawEvents.push({ eventId: patient.pid, laneId: timelineState.type, startTimeMillis: startTimeMillis })
+          }
+        })
+      })
+    } else {
+      lanes.push({
+        laneId: 'timestamp',
+        label: 'Timestamp',
+      })
+      patients.forEach((patient) => {
+        columns.forEach((column) => {
+          if (column.name === 'Timestamp') {
+            const startTimeMillis = Number(patient.values[column.index])
+            rawEvents.push({ eventId: patient.pid, laneId: timelineState.type, startTimeMillis: startTimeMillis })
+          }
+        })
+      })
+    }
+    setRawEvents(rawEvents)
+    setLanes(lanes)
+  }, [timelineState.type, patients, columns])
 
   return (
     <AutoSizer>
