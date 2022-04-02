127367597cd499b95808326916de8afd2e110153
Rahel Lüthy <rahel.luethy@fhnw.ch>
2022-04-02 14:59:02 +0200

    #66: Maximize code reuse in tests

--- patient-journey-client/src/table/tableSlice.test.ts
+++ patient-journey-client/src/table/tableSlice.test.ts
@@ -6,28 +6,33 @@ describe('tableSlice', () => {
   it(`handles ${setSorting.type} action`, async () => {
     const { store, loadedData } = await createStoreWithMockData()
 
-    const ascByFirstPatientColumn: ColumnSortingState = {
+    const eventsDefaultSorting = store.getState().table.events.sorting
+
+    const ASC_BY_FIRST_PATIENT_COLUMN: ColumnSortingState = {
       type: 'asc',
       column: loadedData.patientData.columns[0],
     }
-    store.dispatch(
-      setSorting({
-        view: 'patients',
-        sorting: ascByFirstPatientColumn,
-      })
-    )
-    expect(store.getState().table['patients'].sorting).toEqual(ascByFirstPatientColumn)
 
-    const descByFirstEventColumn: ColumnSortingState = {
+    const DESC_BY_FIRST_EVENT_COLUMN: ColumnSortingState = {
       type: 'desc',
       column: loadedData.eventData.columns[0],
     }
+
+    store.dispatch(
+      setSorting({
+        view: 'patients',
+        sorting: ASC_BY_FIRST_PATIENT_COLUMN,
+      })
+    )
+    expect(store.getState().table['patients'].sorting).toEqual(ASC_BY_FIRST_PATIENT_COLUMN)
+    expect(store.getState().table['events'].sorting).toEqual(eventsDefaultSorting)
     store.dispatch(
       setSorting({
         view: 'events',
-        sorting: descByFirstEventColumn,
+        sorting: DESC_BY_FIRST_EVENT_COLUMN,
       })
     )
-    expect(store.getState().table['events'].sorting).toEqual(descByFirstEventColumn)
+    expect(store.getState().table['patients'].sorting).toEqual(ASC_BY_FIRST_PATIENT_COLUMN)
+    expect(store.getState().table['events'].sorting).toEqual(DESC_BY_FIRST_EVENT_COLUMN)
   })
 })
