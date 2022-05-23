64cccca7071118b751cd4d4e6192df6ec6616bb5
protyze <jan.azzati@gmail.com>
2022-05-23 16:05:32 +0200

    #168: Avoid multiple worker executions

--- patient-journey-client/src/data/workers/hooks.ts
+++ patient-journey-client/src/data/workers/hooks.ts
@@ -1,14 +1,22 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export const useWorker = <D, R>(_Worker: new () => Worker, data: D, initialValue: R): R => {
   const [workerInstance, setWorkerInstance] = useState<Worker>()
   const [result, setResult] = useState<R>(initialValue)
+  const [popQueueToggle, setPopQueueToggle] = useState(false)
+
+  let isBusy = useRef(false)
+  let popData = useRef<any | undefined>(undefined)
 
   // Initialize worker
   useEffect(() => {
     if (!workerInstance) {
       const worker = new _Worker()
-      worker.addEventListener('message', (e) => setResult(e.data))
+      worker.addEventListener('message', (e) => {
+        setResult(e.data)
+        isBusy.current = false
+        setPopQueueToggle((toggle) => !toggle)
+      })
       setWorkerInstance(worker)
     } else {
       return () => {
@@ -20,8 +28,23 @@ export const useWorker = <D, R>(_Worker: new () => Worker, data: D, initialValue
 
   // Post data to the worker when data changes
   useEffect(() => {
-    workerInstance?.postMessage(data)
+    if (workerInstance && isBusy.current === false) {
+      workerInstance.postMessage(data)
+      isBusy.current = true
+    } else if (workerInstance && data) {
+      // Worker is busy, queue data
+      popData.current = data
+    }
   }, [workerInstance, data])
+
+  useEffect(() => {
+    if (workerInstance && popData.current && isBusy.current === false && popQueueToggle !== undefined) {
+      // Worker is not busy anymore, catch up with latest data
+      isBusy.current = true
+      workerInstance.postMessage(popData.current)
+      popData.current = undefined
+    }
+  }, [popQueueToggle, workerInstance])
 
   return result
 }
