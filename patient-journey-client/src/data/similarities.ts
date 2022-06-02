15b5a5ce10b07dadcf2dc3d0f21919777d9e4760
protyze <jan.azzati@gmail.com>
2022-06-02 10:55:42 +0200

    #169: Fix 416 http code error when reading a file in chunks

--- patient-journey-client/src/data/similarities.ts
+++ patient-journey-client/src/data/similarities.ts
@@ -3,6 +3,8 @@ import * as csvParser from 'papaparse'
 import { SIMILARITY_DATA_FILE_URL } from './loading'
 import { PatientData, PatientId } from './patients'
 
+import { getChunkSize } from '../utils'
+
 export type SimilaritiesStateLoadingPending = Readonly<{
   type: 'loading-pending'
 }>
@@ -37,13 +39,17 @@ export type SimilarityData = {
 export const createPatientIdToSimilarityIndexMap = (patients: PatientData['allEntities']): Map<PatientId, number> =>
   new Map<PatientId, number>(patients.map((p, idx) => [p.pid, idx]))
 
-export const parseSpecificRowFromSimilarityFile = (
+export const parseSpecificRowFromSimilarityFile = async (
   rowIndex: number,
   onLoadingDataComplete: (data: LoadedSimilarities) => void,
   onLoadingDataFailed: (message: string) => void
 ) => {
   let currentRow = 0
   let result: string[] = []
+
+  // Avoid this bug: https://github.com/mholt/PapaParse/issues/831
+  const fileSize = await getRemoteFileSize(SIMILARITY_DATA_FILE_URL)
+  const chunkSize = getChunkSize(fileSize, 500)
 
   csvParser.parse<Array<string>>(`${window.location.href}${SIMILARITY_DATA_FILE_URL}`, {
     download: true,
@@ -60,5 +66,15 @@ export const parseSpecificRowFromSimilarityFile = (
         currentRow++
       }
     },
+    header: false,
+    skipEmptyLines: true,
+    delimiter: ',',
+    fastMode: true,
+    chunkSize,
   })
+}
+
+const getRemoteFileSize = async (url: string) => {
+  const res = await fetch(url, { headers: { Range: 'bytes=0-1' } })
+  return +(res.headers.get('content-range')?.split('/')[1] ?? 0)
 }
--- patient-journey-client/src/utils/index.ts
+++ patient-journey-client/src/utils/index.ts
@@ -5,3 +5,12 @@ export const noOp = () => {}
 export const diff = (value1: number, value2: number): number => {
   return Math.abs(Math.max(value1, value2) - Math.min(value1, value2))
 }
+export const getChunkSize = (total: number, chunks: number): number => {
+  let _chunks = chunks
+
+  while (total % _chunks !== 0) {
+    _chunks--
+  }
+
+  return total / _chunks
+}
