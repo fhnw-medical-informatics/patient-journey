a4e7779ad5b697a01dd145a58382e299beb60f33
protyze <jan.azzati@gmail.com>
2022-12-05 14:51:19 +0100

    #197: Adjust light theme

--- patient-journey-client/src/theme/useCustomTheme.ts
+++ patient-journey-client/src/theme/useCustomTheme.ts
@@ -1,6 +1,6 @@
 import { createTheme, darken, lighten, Theme } from '@mui/material'
 import { useAppSelector } from '../store'
-import { yellow, grey, pink, red } from '@mui/material/colors'
+import { yellow, grey, pink, red, amber } from '@mui/material/colors'
 import { AppTheme } from './themeSlice'
 
 declare module '@mui/material/styles' {
@@ -34,15 +34,15 @@ const createCustomTheme = (mode: AppTheme, selectionColor: string, defaultColor:
     palette: {
       mode,
       background: {
-        default: mode === 'light' ? grey[100] : grey[800],
+        default: mode === 'light' ? grey[200] : grey[800],
         paper: mode === 'light' ? grey[50] : grey[900],
       },
     },
     entityColors: {
       default: defaultColor,
-      filteredOut: mode === 'light' ? grey[200] : grey[800],
+      filteredOut: mode === 'light' ? grey[300] : grey[800],
       selected: selectionColor,
-      indexPatient: mode === 'light' ? pink[600] : yellow[700],
+      indexPatient: mode === 'light' ? amber[500] : yellow[700],
       journeyStroke: selectionColor,
     },
     components: {
@@ -63,10 +63,10 @@ const createCustomTheme = (mode: AppTheme, selectionColor: string, defaultColor:
     },
   })
 
-const LIGHT_SELECTION_COLOR = yellow[400]
+const LIGHT_SELECTION_COLOR = pink[500]
 const DARK_SELECTION_COLOR = red['A200']
 
-const LIGHT_DEFAULT_COLOR = grey[400]
+const LIGHT_DEFAULT_COLOR = grey[500]
 const DARK_DEFAULT_COLOR = grey[600]
 
 export const lightTheme = createCustomTheme('light', LIGHT_SELECTION_COLOR, LIGHT_DEFAULT_COLOR)
@@ -78,7 +78,7 @@ export const useCustomTheme = () =>
   })
 
 export const LIGHTENING_FACTOR = 0.3
-export const DARKENING_FACTOR = 0.6
+export const DARKENING_FACTOR = 0.3
 
 export const createFocusColor = (theme: Theme, color: string) =>
   theme.palette.mode === 'dark' ? lighten(color, LIGHTENING_FACTOR) : darken(color, DARKENING_FACTOR)
