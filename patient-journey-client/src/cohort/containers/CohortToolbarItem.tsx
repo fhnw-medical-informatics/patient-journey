e9b06a6ba92731bd8fa8eab7d69d49fd8ff61600
Rahel Lüthy <rahel.luethy@fhnw.ch>
2023-05-12 11:28:22 +0200

    #235: Add cohort toolbar popover

--- patient-journey-client/src/cohort/containers/CohortToolbarItem.tsx
+++ patient-journey-client/src/cohort/containers/CohortToolbarItem.tsx
@@ -1,7 +1,34 @@
 import { CohortToolbarItem as CohortToolbarItemComponent } from '../components/CohortToolbarItem'
 import { usePatientCohort } from '../hooks'
+import { useCallback } from 'react'
+import { useDispatch } from 'react-redux'
+import { clearCohort, removeFromCohort } from '../cohortSlice'
+import { PatientId } from '../../data/patients'
 
 export const CohortToolbarItem = () => {
-  const count = usePatientCohort().size
-  return <CohortToolbarItemComponent count={count} />
+  const cohort = usePatientCohort()
+
+  const dispatch = useDispatch()
+  const onRemoveFromCohort = useCallback(
+    (id: PatientId) => {
+      dispatch(removeFromCohort({ id }))
+    },
+    [dispatch]
+  )
+  const onClearCohort = useCallback(() => {
+    dispatch(clearCohort())
+  }, [dispatch])
+
+  const onCopyToClipboard = useCallback(() => {
+    navigator.clipboard.writeText([...cohort].join('\n'))
+  }, [cohort])
+
+  return (
+    <CohortToolbarItemComponent
+      cohort={cohort}
+      onRemoveFromCohort={onRemoveFromCohort}
+      onClearCohort={onClearCohort}
+      onCopyToClipboard={onCopyToClipboard}
+    />
+  )
 }
