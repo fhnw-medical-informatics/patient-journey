faec0abff8b7548bf33200e08ee6e2328d0fb794
protyze <jan.azzati@gmail.com>
2023-08-19 15:12:54 +0200

    #235: Make cohort items interactive

--- patient-journey-client/src/data/containers/cohort/CohortToolbarItem.tsx
+++ patient-journey-client/src/data/containers/cohort/CohortToolbarItem.tsx
@@ -1,12 +1,15 @@
 import { useCallback } from 'react'
 import { useDispatch } from 'react-redux'
-import { usePatientCohort } from '../../hooks'
+import { useActiveSelectedEntity, useEntityInteraction, useHoveredEntity, usePatientCohort } from '../../hooks'
 import { PatientId } from '../../patients'
 import { clearCohort, removeFromCohort } from '../../dataSlice'
 import { CohortToolbarItem as CohortToolbarItemComponent } from '../../components/cohort/CohortToolbarItem'
 
 export const CohortToolbarItem = () => {
   const cohort = usePatientCohort()
+  const { onEntityClick, onEntityHover } = useEntityInteraction('patients')
+  const hoveredEntity = useHoveredEntity()
+  const selectedEntityId = useActiveSelectedEntity()
 
   const dispatch = useDispatch()
   const onRemoveFromCohort = useCallback(
@@ -26,6 +29,10 @@ export const CohortToolbarItem = () => {
   return (
     <CohortToolbarItemComponent
       cohort={cohort}
+      hoveredEntity={hoveredEntity}
+      selectedEntityId={selectedEntityId}
+      onPatientClick={onEntityClick}
+      onPatientHover={onEntityHover}
       onRemoveFromCohort={onRemoveFromCohort}
       onClearCohort={onClearCohort}
       onCopyToClipboard={onCopyToClipboard}
