1aa6df01d242b1b9591c8c9908e945639083ff1d
protyze <jan.azzati@gmail.com>
2024-02-15 14:23:31 +0100

    Provide an openai mock to run tests without api keys

--- patient-journey-client/src/test/setup.ts
+++ patient-journey-client/src/test/setup.ts
@@ -8,3 +8,22 @@ globalAny.fetch = () => ({
   text: () => Promise.resolve(''),
 })
 globalAny.Response = Response
+
+const mockOpenAI = jest.fn().mockImplementation(() => ({
+  organization: 'mocked_organization',
+  apiKey: 'mocked_api_key',
+  dangerouslyAllowBrowser: true,
+  beta: {
+    assistants: {
+      retrieve: jest.fn().mockResolvedValue({
+        id: 'mocked_assistant_id',
+      }),
+    },
+  },
+}))
+
+jest.mock('openai', () => {
+  return {
+    default: mockOpenAI,
+  }
+})
